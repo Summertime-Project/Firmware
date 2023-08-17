a05fc06bbfbedaa34fff57c485cfa284effe73fe
romannowak9 <roman.nowak9@interia.pl>
2023-08-18 01:01:49 +0200

    poprawki w arm.c

--- Include/arm.h
+++ Include/arm.h
@@ -1,6 +1,12 @@
 #ifndef ARM_H
 #define ARM_H
 
-double **reverseKinematics(double x, double y, double z);
+#include "Utils.h"
+
+#define P1 15  // Długości członów w tych samych jednostach, w których będą zadawane wartości położenia końcówki x, y, z
+#define P2 15
+#define P3 10
+
+ErrorCode_t **reverseKinematics(**float outTab, double x, double y, double z);
 
 #endif //ARM_H
--- Source/arm.c
+++ Source/arm.c
@@ -1,42 +1,47 @@
 #include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include "Utils.h"
 #include "arm.h"
 
-#define P1 15  // Długości członów w tych samych jednostach, w których będą zadawane wartości położenia końcówki x, y, z
-#define P2 15
-#define P3 10
+/**
+    Funkcja przyjmująca współrzędne kartezjańskie końcówki robota RRR,
+    i obliczająca współrzędne złączowe - kąty serw w stopniach
 
-// Może zmienić na float? nie wiem w sumie
-// Funkcja przyjmująca współrzęne kartezjańskie końcówki robota RRR, a zwracająca współrzędne złączowe - kąty serw w stopniach,
-// w postaci [[t1a, t2a, t3a], [t1b, t2b, t3b]] - istnieją dwa rozwiązania, w przypadku gdy zadany punktu jest poza zasięgiem
-// ramienia zwraca NULL
-double **reverseKinematics(double x, double y, double z){
-    double** outCords = malloc(2 * sizeof(double*));
-    for (int i = 0; i < 2; ++i)
-        outCords[i] = malloc(3 * sizeof(double));
+    @param outTab: float**, dwuwymiarowa tablica o rozmiarze [2, 3] (!) rozwiązań
+        w postaci [[t1a, t2a, t3a], [t1b, t2b, t3b]] - istnieją dwa rozwiązania,
+        w przypadku gdy zadany punkt jest poza zasięgiem ramienia przyjmuje NULL
+    @param x: float, współrzędna kartezjańska osi x
+    @param y: float, współrzędna kartezjańska osi y
+    @param z: float, współrzędna kartezjańska osi z
 
-    double theta1 = atan2(y, x);
-    double cost3 = (pow(z - P1, 2) + pow(x, 2) + pow(y, 2) - pow(P2, 2) - pow(P3, 2)) / (2 * P2 * P3);
+    @return kod błędu: ErrorCode_t; E_OK, gdy sukces, E_WRONGINPUT, gdy wartości
+        x, y lub z spoza zasięgu ramienia
+**/
+ErrorCode_t **reverseKinematics(float** outTab, float x, float y, float z){
+    float theta1 = atan2(y, x);
+    float cost3 = (pow(z - P1, 2) + pow(x, 2) + pow(y, 2) - pow(P2, 2) - pow(P3, 2)) / (2 * P2 * P3);
+
     // Punkt poza zasięgiem ramiania
-    if (cost3 > 1)
-        return NULL;
+    if (cost3 > 1) {
+        outTab = NULL;
+        return E_WRONGINPUT;
+    }
 
-    double sint3_a = sqrt(1 - pow(cost3, 2));
-    double sint3_b = -sint3_a;
-    double theta3_a = atan2(sint3_a, cost3);
-    double theta3_b = atan2(sint3_b, cost3);
-    double beta = atan2(z - P1, sqrt(pow(x,2) + pow(y, 2)));
-    double theta2_a = beta + atan2(P3 * sint3_a, P2 + P3 * cost3);
-    double theta2_b = beta + atan2(P3 * sint3_b, P2 + P3 * cost3);
+    float sint3_a = sqrt(1 - pow(cost3, 2));
+    float sint3_b = -sint3_a;
+    float theta3_a = atan2(sint3_a, cost3);
+    float theta3_b = atan2(sint3_b, cost3);
+    float beta = atan2(z - P1, sqrt(pow(x,2) + pow(y, 2)));
+    float theta2_a = beta + atan2(P3 * sint3_a, P2 + P3 * cost3);
+    float theta2_b = beta + atan2(P3 * sint3_b, P2 + P3 * cost3);
 
-    outCords[0][0] = theta1 * (180 / M_PI);
-    outCords[1][0] = theta1 * (180 / M_PI);
-    outCords[0][1] = theta2_a * (180 / M_PI);
-    outCords[1][1] = theta2_b * (180 / M_PI);
-    outCords[0][2] = theta3_a * (180 / M_PI);
-    outCords[1][2] = theta3_b * (180 / M_PI);
+    // Przeliczenie na stopnie
+    outTab[0][0] = theta1 * (180 / M_PI);
+    outTab[1][0] = theta1 * (180 / M_PI);
+    outTab[0][1] = theta2_a * (180 / M_PI);
+    outTab[1][1] = theta2_b * (180 / M_PI);
+    outTab[0][2] = theta3_a * (180 / M_PI);
+    outTab[1][2] = theta3_b * (180 / M_PI);
 
-    return outCords;
+    return E_OK;
 }
-
